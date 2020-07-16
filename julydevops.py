01deb1f789f8c46ed521b52a64d4ac29c6fd6915
Chris B <chrisburd91@gmai.com>
2020-07-16 17:21:48 +0100

    Uploading challenge1 otd solutions

---
+++ julydevops.py
@@ -0,0 +1,14 @@
+July_Devops_Cohort = ["Chris", "Chris" , "Josh", "Tobi", "Sam", "Jason", "Mowafak", 
+"John", "Wasim", "Ryan", "Domenico", "Bradley", 
+"Javas", "Sith", "Jacob", "Clifford" ,
+ "Ed", "Steven", "Jack"]
+
+
+July_Devops_Cohort.append("Luke") 
+
+print(July_Devops_Cohort)
+
+
+print(July_Devops_Cohort[5])  #What is special is that this is actually the 6th person along as we start counting from 0(where zero takes the 1st position in the list)
+
+print(July_Devops_Cohort.count("Chris"))

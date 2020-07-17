585b1decd8ed0ddc1cafba1beccd6d3b7be2a8a7
Chris B <chrisburd91@gmai.com>
2020-07-17 16:11:21 +0100

    upload again as file changes were made before inital upload

--- timestable.py
+++ timestable.py
@@ -1,8 +1,7 @@
-n = int(input("Please insert number:")) + 1
-
+n = int(input("Please insert number:")) 
+n += 1
 
 for i in range(1,n):
     for j in range(1,n):
         print("{:3}".format(i*j), end=" ")
     print()
-n += 1

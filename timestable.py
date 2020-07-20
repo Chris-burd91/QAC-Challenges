62fbd320c8abc4607293cc5fae007cb22d3dc347
Chris B <chrisburd91@gmai.com>
2020-07-20 01:16:16 +0100

    Final change to challenge

--- timestable.py
+++ timestable.py
@@ -1,7 +1,18 @@
-n = int(input("Please insert number:")) 
-n += 1
 
-for i in range(1,n):
-    for j in range(1,n):
-        print("{:3}".format(i*j), end=" ")
-    print()
+def timestable():
+  n = int(input("Please insert number:")) 
+  for i in range(1,n+1):
+      for j in range(1,n+1):
+        print("{:4}".format(i*j), end="\t")
+      print('\n')
+   
+
+
+timestable()
+
+
+
+
+
+
+

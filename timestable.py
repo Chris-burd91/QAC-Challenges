0471fd5fed476b55e83f6cd82536eca3859a8177
Chris B <chrisburd91@gmai.com>
2020-07-20 21:21:22 +0100

    Solution to timestable challenge

--- timestable.py
+++ timestable.py
@@ -1,14 +1,23 @@
 
+#def timestable():
+ # n = int(input("Please insert number:")) 
+  #for i in range(1,n+1):
+   #   for j in range(1,n+1):
+    #    print("{:3}".format(i*j), end="\t")
+     # print('\n')
+
 def timestable():
   n = int(input("Please insert number:")) 
+  output = ""
   for i in range(1,n+1):
       for j in range(1,n+1):
-        print("{:4}".format(i*j), end="\t")
-      print('\n')
+        output += (str(i*j)) + "\t"
+      output += "\n" + "\n"
+  return output
    
 
 
-timestable()
+print(timestable())
 
 
 

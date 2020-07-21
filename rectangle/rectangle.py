1373ebc70f564244b8c3321b02a438ed743b93fe
Chris B <chrisburd91@gmai.com>
2020-07-21 17:54:05 +0100

    Challenge of the day for rectangle

---
+++ rectangle/rectangle.py
@@ -0,0 +1,15 @@
+
+
+class Rectangle():
+
+    def __init__(self,h,l):
+        self.height = h
+        self.length = l
+
+    def rectangle_area(self):
+        return self.length*self.height
+
+    
+create_Rectangle = Rectangle(3, 6)
+
+print(create_Rectangle.rectangle_area())
---
+++ rectangle/rectangle_test.py
@@ -0,0 +1,5 @@
+import pytest
+import rectangle
+
+def test_3_by_6():    
+    assert rectangle.create_Rectangle.rectangle_area() == 18
